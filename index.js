8c701537e06967223e000cd3858264e122462861
megos <gmegos.27@gmail.com>
2020-10-31 13:46:12 +0900

    Move pins

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const gravity = new THREE.Vector3(0, -GRAVITY, 0).multiplyScalar(MASS);
 const TIMESTEP = 18 / 1000;
 const TIMESTEP_SQ = TIMESTEP * TIMESTEP;
 
-let pins = [];
+const pins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const windForce = new THREE.Vector3(0, 0, 0);
 
@@ -250,8 +250,6 @@ function simulate(now) {
 }
 
 /* testing cloth simulation */
-
-pins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let container, stats;
 let camera, scene, renderer;
